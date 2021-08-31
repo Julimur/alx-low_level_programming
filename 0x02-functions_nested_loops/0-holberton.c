4aef036fd1b7dff1b27ced70069f308661006fbf
Julimur <murigijulius1@gmail.com>
2021-08-31 13:11:55 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	char a[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Functions nesyed loops
