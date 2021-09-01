af830d0c4253776ed2a0479398cce2ab0b913387
Julimur <murigijulius1@gmail.com>
2021-09-01 14:15:36 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,90 +1,46 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_to_98 - prints numbers to 98
- * @n: first parameter
+ *print_to_98 - natural numbers from n to 98.
+ *@n: input.
  *
- * Description: prints all num to 98
- * Return: Always (0).
+ *Return: void.
+ *
  */
-
 void print_to_98(int n)
 {
-	int end;
-
-	end = 98;
-
-	if (n < end)
+	if (n == 98)
 	{
-		for (; n <= end; n++)
+		printf("%d\n", n);
+	}
+	else if (n < 98)
+	{
+		int j;
+		for (j = n; j < 99; j++)
 		{
-			if (n == 98)
+			if (j == 98)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				break;
+				printf("%d\n", 98);
 			}
 			else
 			{
-				if (n >= -9 && n <= 9)
-				{
-					if (n < 0)
-					{
-						_putchar('-');
-						_putchar((n * -1) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-					else if (n >= 0)
-					{
-						_putchar(n + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else if (n >= 100)
-				{
-					_putchar((n / 100) + '0');
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-					_putchar(' ');
-				}
+				printf("%d, ", j);
 			}
 		}
-		_putchar('\n');
 	}
-	else if (n == 0)
+	else if (n > 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar('\n');
-	}
-	else
-	{
-		for (; n >= end; n--)
+		int j;
+		for (j = n; j > 97; j--)
 		{
-			if (n == 98)
+			if (j == 98)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				break;
+				printf("%d\n", j);
 			}
 			else
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", j);
 			}
 		}
-		_putchar('\n');
 	}
-	return;
 }
