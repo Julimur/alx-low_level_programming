ac1c555e804e8ce6dd3d5a961231de03a9446a17
Julimur <murigijulius1@gmail.com>
2021-09-01 11:50:52 -0700

    update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 /**
- *main - Entry Point
- *
- *Return: Always 0.
- */
+*main - prints Holberton
+*Return: 0
+*/
 int main(void)
 {
-	char a[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(a[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
