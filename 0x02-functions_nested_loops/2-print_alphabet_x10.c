12725b6b3a650b6ce22f7ffe25c2b5a45371e73b
Julimur <murigijulius1@gmail.com>
2021-09-01 13:39:28 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,18 +11,14 @@ void print_alphabet_x10(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	
-	int i, j;
-	
-	i = 0;
-	
+	int i, j;	
+	i = 0;	
 	while (i < 10)
 	{
-		j = 0;
-		
+		j = 0;		
 		while (j < 26)
 		{
-			_putchar(alphabet[j]);
-			
+			_putchar(alphabet[j]);			
 			j++;
 		}
 		_putchar('\n');
